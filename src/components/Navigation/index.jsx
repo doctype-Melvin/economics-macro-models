515ee48bf23e0ee5ee09d0d1649bd3fccb620c53
doctype-Melvin <doctype.melvin@gmail.com>
2023-07-11 10:46:45 +0200

    add navigation toggle

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,33 +1,50 @@
 import { styled } from "styled-components";
+import { GiHamburgerMenu as Burger } from "react-icons/gi";
+import { TfiClose as CloseIcon } from "react-icons/tfi";
+import { useState } from "react";
 
 export default function Navigation() {
+	const [expandNavigation, setExpandNavigation] = useState(false);
+
+	const handleToggle = () => setExpandNavigation(!expandNavigation);
+
 	return (
 		<StyledNavigation>
-			<StyledNavigationLink href="#" aria-label="Link to Classical Model">
-				Classical
-			</StyledNavigationLink>
-			<StyledNavigationLink href="#" aria-label="Link to Keynesian Model">
-				Keynesian
-			</StyledNavigationLink>
-			<StyledNavigationLink href="#" aria-label="Link to IS-LM Model">
-				IS-LM
-			</StyledNavigationLink>
-			<StyledNavigationLink href="#" aria-label="Link to AS-AD Model">
-				AS-AD
-			</StyledNavigationLink>
-			<StyledNavigationLink href="#" aria-label="Link to New Keynesian Model">
-				New Keynesian
-			</StyledNavigationLink>
-			<StyledNavigationLink href="#" aria-label="Link to DSGE Model">
-				DSGE
-			</StyledNavigationLink>
+			{expandNavigation && (
+				<>
+					<StyledNavigationLink href="#" aria-label="Link to Classical Model">
+						Classical
+					</StyledNavigationLink>
+					<StyledNavigationLink href="#" aria-label="Link to Keynesian Model">
+						Keynesian
+					</StyledNavigationLink>
+					<StyledNavigationLink href="#" aria-label="Link to IS-LM Model">
+						IS-LM
+					</StyledNavigationLink>
+					<StyledNavigationLink href="#" aria-label="Link to AS-AD Model">
+						AS-AD
+					</StyledNavigationLink>
+					<StyledNavigationLink
+						href="#"
+						aria-label="Link to New Keynesian Model"
+					>
+						New Keynesian
+					</StyledNavigationLink>
+					<StyledNavigationLink href="#" aria-label="Link to DSGE Model">
+						DSGE
+					</StyledNavigationLink>
+				</>
+			)}
+			<ToggleButton onClick={handleToggle}>
+				{expandNavigation ? <CloseIcon /> : <Burger />}
+			</ToggleButton>
 		</StyledNavigation>
 	);
 }
 
 const StyledNavigation = styled.nav`
 	display: grid;
-	grid-template-columns: 100%;
+	grid-template-columns: 1fr 0.2fr;
 	grid-template-rows: auto;
 	background-color: lightgray;
 	padding: 0.35rem;
@@ -37,4 +54,13 @@ const StyledNavigation = styled.nav`
 const StyledNavigationLink = styled.a`
 	text-decoration: none;
 	color: black;
+	grid-column: 1 / 2;
+`;
+
+const ToggleButton = styled.button`
+	background-color: transparent;
+	font-size: 1.5rem;
+	border: none;
+	grid-column: 2 / 3;
+	grid-row: 1 / 3;
 `;
